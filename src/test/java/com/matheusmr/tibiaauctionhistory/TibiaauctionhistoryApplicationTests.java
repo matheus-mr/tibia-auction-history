409f713c867fdc9f1bfba811bfd9a1a2c65f0d31
Matheus Martins <matheusmr.95@protonmail.com>
2022-08-20 21:38:18 -0300

    initial commit

---
+++ src/test/java/com/matheusmr/tibiaauctionhistory/TibiaauctionhistoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusmr.tibiaauctionhistory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TibiaauctionhistoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
